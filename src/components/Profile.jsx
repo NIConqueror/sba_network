7eb3061d1cab3c5bda648a79c3b996d364b0182b
NIConqueror <npokhodenko@yandex.by>
2019-09-16 12:15:18 +0300

    commit nomber one

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Profile(){
+    return  <div className='content'>
+    <img src='https://static.scientificamerican.com/sciam/cache/file/EAF12335-B807-4021-9AC95BBA8BEE7C8D_source.jpg?w=590&h=800&74A94564-944F-4349-96BD18788411EAA6'></img>
+    <div>
+    ava + description
+    </div>
+    <div>
+      My post
+      <div>
+        New post
+        </div>
+        <div>
+          post 1
+          </div>
+          <div>
+            post 2  
+          </div>
+    </div>
+  </div>
+}
+
+export default Profile;
