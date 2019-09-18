0bb77193d57f751d8f65dd55960270ad37c7a6c9
NIConqueror <npokhodenko@yandex.by>
2019-09-18 13:59:34 +0300

    nomber four

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import '../components/Navbar.css'
 import classes from './Navbar.module.css'
 
+let classes ={
+ 'nav' : 'Navbar_nav__2dZG',
+ 'item' : 'Navbar_item__3Z5_r',
+}
+
+
 function Navbar(){
-    return  <nav className='nav'>
-    <div className='item'>
+    return  <nav className={classes.nav}>
+    <div className={classes.item}>
       <a> Profile</a>
     </div>
-    <div className='item'>
+    <div className={classes.item}>
       <a>Message</a>
     </div>
-    <div className='item'>
+    <div className={classes.item}>
       <a>News</a>
     </div>
-    <div className='item'>
+    <div className={classes.item}>
      <a> Music</a>
     </div>
-    <div className='item'>
+    <div className={classes.item}>
      <a> Settings</a>
     </div>
   </nav>
